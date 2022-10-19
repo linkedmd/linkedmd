24469d9169ef0f965ffa28b93cc63c18c3709252
Luis Cuende <me@luisivan.net>
2022-10-19 10:58:45 +0000

    fix(): Remove unneeded escaping in styles.css

--- parser/index.ts
+++ parser/index.ts
@@ -2,7 +2,7 @@ const VARIABLE_REGEX = /\[%(.*?)\]/g
 const IPFS_GATEWAY = 'ipfs.nftstorage.link'
 const DEFAULT_VIEWER = 'https://linked.md/v?u='
 const CSS =
-  '<style scoped>dt:target a {outline:2px solid yellow}body{counter-reset:h1counter h2counter h3counter h4counter h5counter h6counter;}h1{counter-reset:h2counter;}h2{counter-reset:h3counter;}h3{counter-reset:h4counter;}h4{counter-reset:h5counter;}h5{counter-reset:h6counter;}h6{}h2:before{counter-increment:h2counter;content:counter(h2counter)".\\0000a0\\0000a0";}h3:before{counter-increment:h3counter;content:counter(h2counter)"."counter(h3counter)".\\0000a0\\0000a0";}h4:before{counter-increment:h4counter;content:counter(h2counter)"."counter(h3counter)"."counter(h4counter)".\\0000a0\\0000a0";}h5:before{counter-increment:h5counter;content:counter(h2counter)"."counter(h3counter)"."counter(h4counter)"."counter(h5counter)".\\0000a0\\0000a0";}h6:before{counter-increment:h6counter;content:counter(h2counter)"."counter(h3counter)"."counter(h4counter)"."counter(h5counter)"."counter(h6counter)".\\0000a0\\0000a0";}body {scroll-behavior: smooth;}.table-of-contents ol { counter-reset:items;}.table-of-contents li {display:block; counter-increment: items;}.table-of-contents li:before {content:counters(items,".") ". ";}.table-of-contents > ol {padding-left: 0 !important;}</style>'
+  '<style scoped>@import url("https://cdn.jsdelivr.net/npm/@linkedmd/parser/styles.css")</style>'
 
 import fetch from 'cross-fetch'
 import MarkdownIt from 'markdown-it'
